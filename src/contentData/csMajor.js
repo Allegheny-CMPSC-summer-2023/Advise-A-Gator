eb51a3c64e6c5f42e8f31d7201a39670cb10c54a
Ochirsaikhan <davaajambal01@allegheny.edu>
2023-07-07 12:11:11 -0400

    Add missing parts to CMPSC Major button to display

--- src/contentData/csMajor.js
+++ src/contentData/csMajor.js
@@ -4,6 +4,14 @@ export default {
     description:
       "Computer Science is associated with Mathematics and Natural Sciences. Students who major in Computer Science may not double-major or minor in Data Science, Informatics, or Software Engineering. The Computer Science major leads to the Bachelor of Science degree and requires a minimum of 48 semester credit hours. To graduate with a major in Computer Science, a student must have an earned GPA of at least 2.0 in required Computer Science and other courses presented for the major. At most one of the foundation courses (CMPSC 100, CMPSC 101, or CMPSC 102) may be presented for the major on the Credit/No Credit grade basis. Students who are interested in Mathematics and/or planning to attend graduate school in Computer Science are strongly encouraged to take Math 151 early in their academic career.",
     distributionRequirements: [],
+    credits: "",
+    prerequisites: {
+      anyof: [],
+      allof: [],
+    },
+    unlocks: [],
+    technicalTopics: [],
+    professionalTopics: [],
     tools: [
       {
         iconType: "book",
@@ -11,6 +19,9 @@ export default {
         href: "https://www.cs.allegheny.edu/teaching/bulletin/#cmpsc",
       },
     ],
+    paltform: ["https://www.cs.allegheny.edu/"],
+    learningObjectives: [],
+    learningOutcomes: [],
   },
   "CMPSC 304": {
     title: "Robotic Agents",
