6f131c511dccf829ae677ae629888534420e69c7
Ochirsaikhan <davaajambal01@allegheny.edu>
2023-07-10 02:51:04 -0400

    Add foundation message

--- src/components/Roadmap/contentData/infMajor.js
+++ src/components/Roadmap/contentData/infMajor.js
@@ -23,6 +23,30 @@ export default {
     learningObjectives: [],
     learningOutcomes: [],
   },
+  Foundation: {
+    title: "Informatics Major",
+    description:
+      "Because this program is Interdisciplinary Studies, students who major in Informatics may complete any minor to satisfy the college requirement that the major and minor be in different areas of study. However, students who major in Informatics may not double-major or minor in Computer Science, Data Science, or Software Engineering. The Informatics major leads to the Bachelor of Arts or the Bachelor of Science degree (students may elect to receive either degree) and requires a minimum of 48 semester credit hours. At most one of the foundation courses (CMPSC 100, CMPSC 101, or CMPSC 105) may be presented for the major on the Credit/No Credit grade basis. Courses with prerequisites are marked with an asterisk (*).",
+    distributionRequirements: [],
+    credits: "",
+    prerequisites: {
+      anyof: [],
+      allof: [],
+    },
+    unlocks: [],
+    technicalTopics: [],
+    professionalTopics: [],
+    tools: [
+      {
+        iconType: "book",
+        title: "Allegheny College Computer Science Academic Bulletin",
+        href: "https://www.cs.allegheny.edu/teaching/bulletin/#cmpsc",
+      },
+    ],
+    platform: ["https://www.cs.allegheny.edu/"],
+    learningObjectives: [],
+    learningOutcomes: [],
+  },
   "Application Modules": {
     title: "Application Modules",
     description:
