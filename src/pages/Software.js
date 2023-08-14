560be9d43308cb8a9ddaaad456c048977702b3fb
BillOchieng <ochieng01@allegheny.edu>
2023-08-14 10:05:31 -0400

    add seMajorblock

--- src/pages/Software.js
+++ src/pages/Software.js
@@ -1,8 +1,91 @@
-export default function Software() {
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+
+import classes from "../components/Roadmap/layout/MainContainer.module.css";
+import boxesData from "../components/Roadmap/boxesData";
+import Modal from "../components/Roadmap/modal/Modal";
+import CartContext from "../components/Roadmap/modal/modalContext";
+import contentData from "../components/Roadmap/contentData";
+import Loader from "../components/Roadmap/layout/Loader";
+import Footer from "../components/Roadmap/layout/Footer";
+import SoftwareEngineeringMajorBlock from "../components/Roadmap/components/SoftwareEngineeringMajorBlock";
+
+const Lines = () => {
+  return (
+    <React.Fragment>
+      <svg
+        width="1280"
+        height="3452"
+        viewBox="0 0 1280 3452"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M957 165.5H858.75C852.537 165.5 847.5 170.537 847.5 176.75V176.75C847.5 182.963 842.463 188 836.25 188H738"
+          stroke="#CED4DA"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+        />
+        <path
+          d="M465.5 318V212C465.5 198.745 476.245 188 489.5 188H543"
+          stroke="#CED4DA"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+        />
+        <path
+          d="M323 255.5H409C422.255 255.5 433 244.755 433 231.5V212C433 198.745 443.745 188 457 188H543"
+          stroke="#CED4DA"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+        />
+        <path
+          d="M813.5 318V212C813.5 198.745 802.755 188 789.5 188H738"
+          stroke="#CED4DA"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+        />
+      </svg>
+    </React.Fragment>
+  );
+};
+
+const SoftwareEngineeringMajorContainer = () => {
+  return (
+    <div className={classes["main-container"]}>
+      <svg
+        viewBox="0 0 1280 1200"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <SoftwareEngineeringMajorBlock boxData={boxesData["seMajor"]} />
+        {/* <Lines /> */}
+      </svg>
+    </div>
+  );
+};
+
+function SoftwareEngineeringMajor() {
+  const { currentTopicTitle } = useContext(CartContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 100);
+  }, []);
+
+  const topicData = contentData[currentTopicTitle];
+
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <h1>Software Engineering Major</h1>
+      <Modal topicData={topicData} />
+      <main>
+        {isLoading && <Loader />}
+        <SoftwareEngineeringMajorContainer />
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
+
+export default SoftwareEngineeringMajor;
